1c860d8be8070b8215305caff0baa60ae304f1f4
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2025-01-16 13:50:08 -0500

    adding rough animation for project listing

---
+++ assets/js/roughProject.js
@@ -0,0 +1,16 @@
+import { annotate } from "rough-notation";
+import { generateColor } from "./colors.js";
+
+const links = document.querySelectorAll(".links");
+
+links.forEach((ln, index) => {
+  const annotation = annotate(ln, {
+    type: "highlight",
+    // color: colors[index + 2],
+    color: generateColor(),
+    padding: 1,
+  });
+  setTimeout(() => {
+    annotation.show();
+  }, (index + 2) * 750);
+});
